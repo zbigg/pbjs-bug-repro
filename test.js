0dc9a50eb9e2d4fdbab78bbc35e63933c5c2d453
zbigg <z.zagorski@gmail.com>
2018-02-28 12:34:58 +0100

    js version added

---
+++ test.js
@@ -0,0 +1,11 @@
+const foo1 = require('./foo');
+const bar2 = require('./bar');
+
+//import * as foo2 from "./foo"
+//import * as bar2 from "./bar"
+
+console.log(foo1.com);
+console.log(bar2.com);
+
+//console.log(foo2.com);
+//console.log(bar2.com);
